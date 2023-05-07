f51b2e7a1e3103965cac33c49b459d486712c3bf
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-05-06 23:04:10 -0700

    showing a cuadratic function in a graph with matplotlib

---
+++ cuadratic_graph.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+x = np.arange(0, 4.5, 0.5)
+plt.plot(x[:5], x[:5]**2, 'b', label='x^2')
+plt.plot(x[4:], x[4:]**2, 'r--')
+
+plt.savefig('./saved_graphs/first_graph.png', dpi=300)
+
+plt.show()
